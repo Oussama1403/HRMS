db2bf883d1f13f2482a6414c42b6ab5305cdf0af
Oussama1403 <obensassi.03@gmail.com>
2023-07-17 20:15:20 +0100

    update

--- instance/insert.sql
+++ instance/insert.sql
@@ -7,7 +7,7 @@ CREATE TABLE matricules (
 );
 INSERT INTO matricules VALUES(111777,1);
 INSERT INTO matricules VALUES(222333,1);
-INSERT INTO matricules VALUES(654321,1);
+INSERT INTO matricules VALUES(654321,0);
 INSERT INTO matricules VALUES(999666,0);
 CREATE TABLE departements (
 	name VARCHAR(20) NOT NULL, 
@@ -19,20 +19,6 @@ INSERT INTO departements VALUES('Gestion',NULL);
 INSERT INTO departements VALUES('Marketing',NULL);
 INSERT INTO departements VALUES('Vente',NULL);
 INSERT INTO departements VALUES('Informatique',NULL);
-CREATE TABLE demande_conge (
-	demande_id INTEGER NOT NULL, 
-	type_conge VARCHAR, 
-	date_deb DATETIME, 
-	date_fin DATETIME, 
-	motif VARCHAR, 
-	PRIMARY KEY (demande_id)
-);
-CREATE TABLE avance_salaire (
-	demande_id INTEGER NOT NULL, 
-	montant INTEGER, 
-	motif VARCHAR, 
-	PRIMARY KEY (demande_id)
-);
 CREATE TABLE user (
 	matricule INTEGER NOT NULL, 
 	first_name VARCHAR(20), 
@@ -48,8 +34,27 @@ CREATE TABLE user (
 	UNIQUE (email), 
 	FOREIGN KEY(dep_name) REFERENCES departements (name)
 );
-INSERT INTO user VALUES(111777,'Adel','Bougrine','adelbougrine@gmail.com','adel','Marketing','Cité Nour-Gafsa',95123741,NULL);
-INSERT INTO user VALUES(222333,'Oussama','Ben Sassi','obensassi.03@gmail.com','2003','Informatique','Lala-Gafsa',25730171,NULL);
-INSERT INTO user VALUES(654321,'John','Doe','john.2003@gmail.com','john','Informatique','Lala-Gafsa',95713962,NULL);
-INSERT INTO user VALUES(999666,'Ala','Benabdallah','ala.03@gmail.com','ala','Vente','Gsar-Gafsa',43258974,NULL);
+INSERT INTO user VALUES(111777,'Adel','Bougrine','adelbougrine@gmail.com','adel','Marketing','Cité Nour-Gafsa',95001300,1200);
+INSERT INTO user VALUES(222333,'Oussama','Ben Sassi','obensassi.03@gmail.com','2003','Informatique','Lala-Gafsa',25730171,2500);
+INSERT INTO user VALUES(654321,'Ahmed','Ben Ali','ahmed.2003@gmail.com','ahmed','Informatique','Lala-Gafsa',95713962,800);
+INSERT INTO user VALUES(999666,'Ala','Hamed','ala.03@gmail.com','ala','Vente','Gsar-Gafsa',43258974,1200);
+CREATE TABLE demande_conge (
+	matricule INTEGER NOT NULL, 
+	type_conge VARCHAR, 
+	date_deb DATETIME, 
+	date_fin DATETIME, 
+	motif VARCHAR, status BOOLEAN, 
+	PRIMARY KEY (matricule), 
+	FOREIGN KEY(matricule) REFERENCES matricules (matricule)
+);
+INSERT INTO demande_conge VALUES(111777,'Congé occasionnel','2023-07-16 00:00:00.000000','2023-08-16 00:00:00.000000','Voyage',NULL);
+INSERT INTO demande_conge VALUES(222333,'Congé de maladie','2023-07-12 00:00:00.000000','2023-07-31 00:00:00.000000','maladie',1);
+CREATE TABLE avance_salaire (
+	matricule INTEGER NOT NULL, 
+	montant INTEGER, 
+	motif VARCHAR, status BOOLEAN, 
+	PRIMARY KEY (matricule), 
+	FOREIGN KEY(matricule) REFERENCES matricules (matricule)
+);
+INSERT INTO avance_salaire VALUES(222333,700,'maladie',0);
 COMMIT;
